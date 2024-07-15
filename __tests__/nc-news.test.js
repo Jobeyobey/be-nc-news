6f008a13dc5a66e84623f8ae06edf9819fc0fbfa
jobeyobey <hollands.joe@gmail.com>
2024-07-15 12:20:54 +0100

    add test for topic properties

--- __tests__/nc-news.test.js
+++ __tests__/nc-news.test.js
@@ -13,12 +13,38 @@ afterAll(() => {
 });
 
 describe("/api/topics", () => {
-    test("GET200: Responds with an array", () => {
+    test("GET200: sends an array of topics to the client", () => {
         return request(app)
             .get("/api/topics")
             .expect(200)
             .then(({ body }) => {
-                expect(Array.isArray(body.topics)).toBe(true);
+                expect(body.topics).toEqual([
+                    {
+                        description: "The man, the Mitch, the legend",
+                        slug: "mitch",
+                    },
+                    {
+                        description: "Not dogs",
+                        slug: "cats",
+                    },
+                    {
+                        description: "what books are made of",
+                        slug: "paper",
+                    },
+                ]);
+            });
+    });
+    test("GET200: each topic from response has a slug and a description", () => {
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+            .then(({ body }) => {
+                body.topics.forEach((topic) => {
+                    expect(topic).toMatchObject({
+                        description: expect.any(String),
+                        slug: expect.any(String),
+                    });
+                });
             });
     });
 });
