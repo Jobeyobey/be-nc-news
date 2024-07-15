9e1b135e0f8841c600e75c2d6c4b3fd04d0989b5
jobeyobey <hollands.joe@gmail.com>
2024-07-15 13:54:52 +0100

    add /api endpoint

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const { getEndpoints } = require("./controllers/app-controller");
 const { getTopics } = require("./controllers/topics-controller");
 const { serverErrorHandler } = require("./error-handlers");
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
