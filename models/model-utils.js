0eabfd072512a50f0423e04aa0579ab3f63a2743
jobeyobey <hollands.joe@gmail.com>
2024-07-16 12:41:31 +0100

    fixed db.end() issues

--- models/model-utils.js
+++ models/model-utils.js
@@ -1,9 +1,5 @@
 const db = require("../db/connection");
 
-afterAll(() => {
-    db.end();
-});
-
 exports.checkArticleExistsById = (article_id) => {
     return db
         .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
