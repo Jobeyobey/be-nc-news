fe2066c3d43c714f0f0bc2854bf21d38cb7332b5
jobeyobey <hollands.joe@gmail.com>
2024-07-17 12:24:49 +0100

    complete t13. comment_count now included in /api/articles/:article_id response

--- models/articles-model.js
+++ models/articles-model.js
@@ -48,7 +48,16 @@ exports.selectArticles = (topic, sort_by, order) => {
 
 exports.selectArticleById = (article_id) => {
     return db
-        .query("SELECT * from articles WHERE article_id = $1", [article_id])
+        .query(
+            `SELECT articles.*,
+            COUNT(comments.article_id)::INT AS comment_count
+            FROM articles
+            LEFT JOIN comments
+            ON articles.article_id = comments.article_id
+            WHERE articles.article_id = $1
+            GROUP BY articles.article_id`,
+            [article_id]
+        )
         .then(({ rows }) => {
             if (rows.length === 0) {
                 return Promise.reject({
